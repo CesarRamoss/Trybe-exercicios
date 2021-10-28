8ad2b633e11c90d32976a5d67a85ad3da0a8914b
César Ramos <cesar.2023.ramos@gmail.com>
2021-10-28 20:03:43 -0300

    exercicios

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,46 +1,51 @@
 let estados = [
-  "AC",
-  "AL",
-  "AM",
-  "AP",
-  "BA",
-  "CE",
-  "DF",
-  "ES",
-  "GO",
-  "MA",
-  "MG",
-  "MS",
-  "MT",
-  "PA",
-  "PB",
-  "PE",
-  "PI",
-  "PR",
-  "RJ",
-  "RN",
-  "RO",
-  "RR",
-  "RS",
-  "SC",
-  "SE",
-  "SP",
-  "TO",
+	"AC",
+	"AL",
+	"AM",
+	"AP",
+	"BA",
+	"CE",
+	"DF",
+	"ES",
+	"GO",
+	"MA",
+	"MG",
+	"MS",
+	"MT",
+	"PA",
+	"PB",
+	"PE",
+	"PI",
+	"PR",
+	"RJ",
+	"RN",
+	"RO",
+	"RR",
+	"RS",
+	"SC",
+	"SE",
+	"SP",
+	"TO",
 ];
 
 for (let i of estados) {
-  let myOption = document.createElement("option");
-  myOption.id = "estado";
-  myOption.innerText = i;
-  let container = document.querySelector("#estado");
-  container.appendChild(myOption);
+	let myOption = document.createElement("option");
+	myOption.id = "estado";
+	myOption.innerText = i;
+	let container = document.querySelector("#estado");
+	container.appendChild(myOption);
 }
 
-function interrompe(e){
-  e.preventDefault();
+function interrompe(e) {
+	e.preventDefault();
 }
 
-let botaoSubmit = document.querySelector('#prevent');
-botaoSubmit.addEventListener('click',interrompe);
+let botaoSubmit = document.querySelector("#prevent");
+botaoSubmit.addEventListener("click", interrompe);
 
+function limpar(){
+	location.reload();
+}
 
+let botaoLimpar = document.querySelector("#limpar");
+botaoLimpar.addEventListener("click",limpar);
